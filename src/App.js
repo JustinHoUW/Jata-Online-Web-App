c90b3050c63edcb662816aa21da100eef87133ad
JustinHoUW <justinhouw@gmail.com>
2024-02-16 13:10:39 -0800

    Merge branch master into justin-ho

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
-import logo from './logo.svg';
+import React, {useState } from 'react';
+ 
 import './App.css';
-import Home from'./pages/Home'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  } from "react-router-dom";
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootswatch/dist/lux/bootstrap.min.css';
 
-function App() {
+//COMPONENT
+import NavBar from './components/NavBar';
+import {ShoppingCart} from './components/ShoppingCart';
+import Login from './components/LoginWindow';
+import SignUp from './components/SignUp';
+
+//PAGES
+import { About } from './pages/About';
+import { Home } from './pages/Home';
+import Profile from './pages/Profile';
+import { SingleSellPost } from './components/singleSellPost';
+
+const App = () => {  
   return (
-    <div className="App">
-     <Home/>
-    </div>
+    <Router>
+      <div>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/cart" element={<ShoppingCart />} />
+          <Route path="/sellpost/:sellPostID" element={<SingleSellPost />} />
+
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
